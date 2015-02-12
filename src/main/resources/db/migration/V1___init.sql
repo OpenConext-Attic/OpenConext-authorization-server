90c95fb847e01980f74b5b1340fcfd9a4fcc4e0f
Hans Westerbeek <hans@enhance-ict.com>
2015-02-12 20:55:13 +0100

    Correct initial schema. Use blobs like in the example hsql.

--- src/main/resources/db/migration/V1___init.sql
+++ src/main/resources/db/migration/V1___init.sql
@@ -1,46 +1,46 @@
 create table oauth_client_details (
-  client_id VARCHAR(255) PRIMARY KEY,
-  resource_ids VARCHAR(255),
-  client_secret VARCHAR(255),
-  scope VARCHAR(255),
-  authorized_grant_types VARCHAR(255),
-  web_server_redirect_uri VARCHAR(255),
-  authorities VARCHAR(255),
+  client_id VARCHAR(256) PRIMARY KEY,
+  resource_ids VARCHAR(256),
+  client_secret VARCHAR(256),
+  scope VARCHAR(256),
+  authorized_grant_types VARCHAR(256),
+  web_server_redirect_uri VARCHAR(256),
+  authorities VARCHAR(256),
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
   additional_information VARCHAR(4096),
-  autoapprove VARCHAR(255)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  autoapprove VARCHAR(256)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 create table oauth_client_token (
-  token_id VARCHAR(255),
-  token VARCHAR(4096),
-  authentication_id VARCHAR(255) PRIMARY KEY,
-  user_name VARCHAR(255),
-  client_id VARCHAR(255)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  token_id VARCHAR(256),
+  token BLOB,
+  authentication_id VARCHAR(256) PRIMARY KEY,
+  user_name VARCHAR(256),
+  client_id VARCHAR(256)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 create table oauth_access_token (
-  token_id VARCHAR(255),
-  token VARCHAR(4096),
-  authentication_id VARCHAR(255) PRIMARY KEY,
-  user_name VARCHAR(255),
-  client_id VARCHAR(255),
-  authentication VARCHAR(4096),
-  refresh_token VARCHAR(255)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  token_id VARCHAR(256),
+  token BLOB,
+  authentication_id VARCHAR(256) PRIMARY KEY,
+  user_name VARCHAR(256),
+  client_id VARCHAR(256),
+  authentication BLOB,
+  refresh_token VARCHAR(256)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 create table oauth_refresh_token (
-  token_id VARCHAR(255),
-  token VARCHAR(4096),
-  authentication VARCHAR(4096)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  token_id VARCHAR(256),
+  token BLOB(4096),
+  authentication BLOB(4096)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
 
 create table oauth_approvals (
-	userId VARCHAR(255),
-	clientId VARCHAR(255),
-	scope VARCHAR(255),
+	userId VARCHAR(256),
+	clientId VARCHAR(256),
+	scope VARCHAR(256),
 	status VARCHAR(10),
-	expiresAt TIMESTAMP,
+	expiresAt DATETIME,
 	lastModifiedAt TIMESTAMP
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+)ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
