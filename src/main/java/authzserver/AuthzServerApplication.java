b745e34620968454c4c4c38004b79466e9a1e31f
Hans Westerbeek <hans@enhance-ict.com>
2015-02-24 15:05:15 +0100

    Fix CheckTokenEndpoint.

--- src/main/java/authzserver/AuthzServerApplication.java
+++ src/main/java/authzserver/AuthzServerApplication.java
@@ -6,15 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.ApprovalStoreUserApprovalHandler;
 import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
 import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
@@ -35,42 +39,53 @@ public class AuthzServerApplication {
     @Autowired
     private DataSource dataSource;
 
-    @Value("${oauthServer.accessTokenValiditySeconds}")
-    private Integer accessTokenValiditySeconds;
-
-    @Value("${oauthServer.refreshTokenValiditySeconds}")
-    private Integer refreshTokenValiditySeconds;
-
-    @Value("${oauthServer.approvalExpirySeconds}")
-    private Integer approvalExpirySeconds;
-
+    @Autowired
+    private ApprovalStore approvalStore;
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints)
       throws Exception {
+      endpoints
+        .approvalStore(approvalStore)
+        .authorizationCodeServices(new InMemoryAuthorizationCodeServices());
+    }
 
-      final TokenStore tokenStore = new JdbcTokenStore(dataSource);
-      final JdbcApprovalStore approvalStore = new JdbcApprovalStore(dataSource);
-
+    @Bean
+    @Autowired
+    public ApprovalStoreUserApprovalHandler approvalStoreUserApprovalHandler(
+      @Value("${oauthServer.approvalExpirySeconds}") Integer approvalExpirySeconds,
+      ApprovalStore approvalStore, ClientDetailsService clientDetailsService) {
       final ApprovalStoreUserApprovalHandler userApprovalHandler = new ApprovalStoreUserApprovalHandler();
-      userApprovalHandler.setApprovalStore(approvalStore);
       userApprovalHandler.setApprovalExpiryInSeconds(approvalExpirySeconds);
+      userApprovalHandler.setApprovalStore(approvalStore);
 
+      DefaultOAuth2RequestFactory requestFactory = new DefaultOAuth2RequestFactory(clientDetailsService);
+      userApprovalHandler.setRequestFactory(requestFactory);
+      return userApprovalHandler;
+    }
+
+    @Bean
+    public ApprovalStore approvalStore() {
+      return new JdbcApprovalStore(dataSource);
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+      return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    @Autowired
+    public DefaultTokenServices tokenServices(@Value("${oauthServer.accessTokenValiditySeconds}") Integer accessTokenValiditySeconds,
+                                              @Value("${oauthServer.refreshTokenValiditySeconds}") Integer refreshTokenValiditySeconds,
+                                              TokenStore tokenStore) {
       final DefaultTokenServices tokenServices = new DefaultTokenServices();
       tokenServices.setSupportRefreshToken(true);
       tokenServices.setTokenStore(tokenStore);
       tokenServices.setAccessTokenValiditySeconds(accessTokenValiditySeconds);
       tokenServices.setRefreshTokenValiditySeconds(refreshTokenValiditySeconds);
-
-      endpoints
-        .tokenServices(tokenServices)
-        .tokenStore(tokenStore)
-        .approvalStore(approvalStore)
-        .userApprovalHandler(userApprovalHandler)
-        .authorizationCodeServices(new InMemoryAuthorizationCodeServices());
+      return tokenServices;
     }
-
-
 
 
     @Override
