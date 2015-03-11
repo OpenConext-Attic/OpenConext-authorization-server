f637357972e8d00d09fe5d77a5d9a103e37ba7b7
Hans Westerbeek <hans@enhance-ict.com>
2015-03-11 19:52:16 +0100

    Replace Yaml-based config with Hoconf. Fixed setting roles for resourceservers.

--- src/main/java/authzserver/ClientsAndResourcesInitializer.java
+++ src/main/java/authzserver/ClientsAndResourcesInitializer.java
@@ -1,6 +1,8 @@
 package authzserver;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -10,17 +12,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.io.Resource;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientRegistrationService;
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionTemplate;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.SafeConstructor;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigObject;
 
 public class ClientsAndResourcesInitializer implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -40,19 +44,17 @@ public class ClientsAndResourcesInitializer implements ApplicationListener<Conte
   public void onApplicationEvent(ContextRefreshedEvent event) {
     LOG.debug("Initializing default oAuth clients and resource-servers from {}", resource);
     try {
-      final Yaml yaml = new Yaml(new SafeConstructor());
-      @SuppressWarnings("unchecked")
-      Map<String, Object> content = (Map<String, Object>) yaml.load(resource.getInputStream());
+      Config config = ConfigFactory.parseReader(new InputStreamReader(resource.getInputStream()));
 
-      final List<Map<String, Object>> clients = (List<Map<String, Object>>) content.get("clients");
-      final List<Map<String, Object>> resourceServers = (List<Map<String, Object>>) content.get("resourceServers");
+      final List<? extends ConfigObject> clients = config.getObjectList("clients");
+      final List<? extends ConfigObject> resourceServers = config.getObjectList("resourceServers");
 
       new TransactionTemplate(transactionManager).execute((TransactionStatus transactionStatus) -> {
 
           List<ClientDetails> preExisting = clientRegistrationService.listClientDetails();
-
-          clients.forEach(clientYaml -> {
-            final String clientId = (String) clientYaml.get("clientId");
+          clients.forEach(clientConfigObj -> {
+            final Map<String, Object> clientConfig = clientConfigObj.unwrapped();
+            final String clientId = (String) clientConfig.get("clientId");
 
             final Optional<ClientDetails> preExistingClientDetails =
               preExisting.stream()
@@ -61,19 +63,19 @@ public class ClientsAndResourcesInitializer implements ApplicationListener<Conte
 
             BaseClientDetails clientDetails = preExistingClientDetails.isPresent() ? (BaseClientDetails) preExistingClientDetails.get() : new BaseClientDetails(clientId, null, null, null, null);
 
-            final String secret = (String) clientYaml.get("secret");
+            final String secret = (String) clientConfig.get("secret");
             clientDetails.setClientSecret(secret);
 
-            final List<String> resourceIds = (List<String>) clientYaml.get("resourceIds");
+            List<String> resourceIds = (List<String>) clientConfig.get("resourceIds");
             clientDetails.setResourceIds(resourceIds);
 
-            final List<String> scopes = (List<String>) clientYaml.get("scopes");
+            final List<String> scopes = (List<String>) clientConfig.get("scopes");
             clientDetails.setScope(scopes);
 
-            final List<String> grantTypes = (List<String>) clientYaml.get("grantTypes");
+            final List<String> grantTypes = (List<String>) clientConfig.get("grantTypes");
             clientDetails.setAuthorizedGrantTypes(grantTypes);
 
-            final String redirectUri = (String) clientYaml.get("redirectUri");
+            final String redirectUri = (String) clientConfig.get("redirectUri");
             clientDetails.setRegisteredRedirectUri(ImmutableSet.of(redirectUri));
 
             if (preExistingClientDetails.isPresent()) {
@@ -83,9 +85,10 @@ public class ClientsAndResourcesInitializer implements ApplicationListener<Conte
               clientRegistrationService.addClientDetails(clientDetails);
             }
           });
+          resourceServers.forEach(resourceServerConfigObj -> {
 
-          resourceServers.forEach(resourceServerYaml -> {
-            final String clientId = (String) resourceServerYaml.get("clientId");
+            final Map<String, Object> resourceServerConfig = resourceServerConfigObj.unwrapped();
+            final String clientId = (String) resourceServerConfig.get("clientId");
 
             final Optional<ClientDetails> preExistingClientDetails =
               preExisting.stream()
@@ -94,10 +97,10 @@ public class ClientsAndResourcesInitializer implements ApplicationListener<Conte
 
             BaseClientDetails clientDetails = preExistingClientDetails.isPresent() ? (BaseClientDetails) preExistingClientDetails.get() : new BaseClientDetails(clientId, null, null, null, null);
             // always add the token checker role
-            clientDetails.setAuthorizedGrantTypes(ImmutableList.of(AuthzServerApplication.ROLE_TOKEN_CHECKER));
-            final String secret = (String) resourceServerYaml.get("secret");
+            clientDetails.setAuthorities(ImmutableList.of(new SimpleGrantedAuthority(AuthzServerApplication.ROLE_TOKEN_CHECKER)));
+            final String secret = (String) resourceServerConfig.get("secret");
             clientDetails.setClientSecret(secret);
-
+            clientDetails.setAuthorizedGrantTypes(Collections.emptyList());
             if (preExistingClientDetails.isPresent()) {
               clientRegistrationService.updateClientDetails(clientDetails);
               clientRegistrationService.updateClientSecret(clientDetails.getClientId(), secret);
