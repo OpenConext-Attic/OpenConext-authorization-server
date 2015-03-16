5eec9b08b7d1c52ce9a4db258493853697b0703a
oharsta <oharsta@zilverline.com>
2015-03-16 12:55:52 +0100

    Small boy scout rule submit

--- src/test/java/authzserver/CorsFilterTest.java
+++ src/test/java/authzserver/CorsFilterTest.java
@@ -1,33 +1,53 @@
 package authzserver;
 
-import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 import javax.servlet.FilterChain;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.*;
 
-public class CorsFilterTest extends TestCase {
+public class CorsFilterTest {
 
   private CorsFilter subject = new CorsFilter();
+  private MockHttpServletRequest req;
+  private HttpServletResponse res;
+  private FilterChain chain;
+
+
+  @Before
+  public void before() {
+    req = new MockHttpServletRequest();
+    res = new MockHttpServletResponse();
+    chain = mock(FilterChain.class);
+  }
 
   @Test
   public void testDoFilterWithNoPreFlight() throws Exception {
-    HttpServletRequest req = new MockHttpServletRequest();
-    HttpServletResponse res = new MockHttpServletResponse();
-    FilterChain chain = mock(FilterChain.class);
-
     subject.doFilter(req, res, chain);
 
     verify(chain, atLeastOnce()).doFilter(req, res);
 
     assertDefaultCorsHeaders(res);
-
     assertNull(res.getHeader("Access-Control-Allow-Headers"));
+  }
+
+  @Test
+  public void testDoFilterWithPreFlight() throws Exception {
+    String requestedRequestHeader = "Requested Request Headers";
+    req.addHeader("Access-Control-Request-Method", "POST");
+    req.addHeader("Access-Control-Request-Headers", requestedRequestHeader);
+
+    subject.doFilter(req, res, chain);
+
+    verify(chain, never()).doFilter(req, res);
+    assertDefaultCorsHeaders(res);
+    assertEquals(requestedRequestHeader, res.getHeader("Access-Control-Allow-Headers"));
   }
 
   private void assertDefaultCorsHeaders(HttpServletResponse res) {
@@ -35,24 +55,6 @@ public class CorsFilterTest extends TestCase {
     assertEquals("*", res.getHeader("Access-Control-Allow-Methods"));
     assertEquals("true", res.getHeader("Access-Control-Allow-Credentials"));
     assertEquals("1728000", res.getHeader("Access-Control-Max-Age"));
-  }
-
-  @Test
-  public void testDoFilterWithPreFlight() throws Exception {
-    MockHttpServletRequest req = new MockHttpServletRequest("OPTIONS", "http://localhost");
-    HttpServletResponse res = new MockHttpServletResponse();
-    FilterChain chain = mock(FilterChain.class);
-    req.addHeader("Access-Control-Request-Method", "POST");
-    String requestedRequestHeader = "Requested Request Headers";
-    req.addHeader("Access-Control-Request-Headers", requestedRequestHeader);
-
-    subject.doFilter(req, res, chain);
-
-    verify(chain, never()).doFilter(req, res);
-
-    assertDefaultCorsHeaders(res);
-
-    assertEquals(requestedRequestHeader, res.getHeader("Access-Control-Allow-Headers"));
   }
 
 }
